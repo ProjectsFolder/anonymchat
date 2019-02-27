fbc883abec1069b30d280eac3fdd21ee510b5113
neretin-trike <hawktrike@gmail.com>
2019-02-27 21:13:28 +0700

    update(js): добавление возможности создавать команту

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-class SignForm {
+class LoginChatForm {
     constructor(options, callback) {
 
         let fieldsetRegister = document.querySelector(".login-fieldset.sign-up");
@@ -24,40 +24,135 @@ class SignForm {
             return false;
         }    
         elem.elements.changeType.onclick = function() {
-            switch (options.type) {
-                case "registration": {
-                    fieldsetRegister.style.display = "none";
-                    fieldsetLogin.style.display = "block";
-                    break;
-                }
-                case "autorisation": {
-                    fieldsetRegister.style.display = "block";
-                    fieldsetLogin.style.display = "none";
-                    break;
-                }
-            }
+            // switch (options.type) {
+            //     case "registration": {
+            //         fieldsetRegister.style.display = "none";
+            //         fieldsetLogin.style.display = "block";
+            //         break;
+            //     }
+            //     case "autorisation": {
+            //         fieldsetRegister.style.display = "block";
+            //         fieldsetLogin.style.display = "none";
+            //         break;
+            //     }
+            // }
         }
     }
 }
 
+async function getProducts() {
+    let responseProducts = await fetch('http://localhost:8090/products');
+    let products = await responseProducts.json(); // will not work without "await" word
+
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000)); // this works like Sleep(3000) in threads
+    console.log(JSON.stringify(products, '', ' '));
+    let responseFirstProduct = await fetch('http://localhost:8090/products/1');
+    let firstProduct = await responseFirstProduct.json();
+    console.log(JSON.stringify(firstProduct, '', ' '));
+    return products;
+  }
+//   getProducts();
+
 
 window.onload = function () {
+    // let formRegister = new LoginChatForm({type:"registration",apiRoute:"register"}, 
+    //     (xhr, elem) => alert("Успешно") 
+    // );
 
-    let formRegister = new SignForm({type:"registration",apiRoute:"register"}, 
-        (xhr, elem) => alert("Успешно") 
-    );
+    let url_string = window.location.href;
+    let url = new URL(url_string);
+    let key = url.searchParams.get("key");
 
-    let formLogin = new SignForm({type:"autorisation",apiRoute:"login"}, 
-        (xhr, elem) => {
-                let respObj = JSON.parse(xhr.responseText);
+    let loginChatForm = document.getElementsByName("loginChat")[0];
 
-                sessionStorage.setItem('token', respObj.token);
-                sessionStorage.setItem('userid', respObj.userid);
-                sessionStorage.setItem('login', elem.login.value);
+    function createChatSusccess(xhr) {
+        let respObj = JSON.parse(xhr.response);
+        console.dir(respObj);
 
-                elem.submit();
+        loginChatForm.roomName.disabled = true;
+        loginChatForm.userName.disabled = false;
+        
+        loginChatForm.nextButton.disabled = true;
+
+        sessionStorage.setItem('key', respObj.key);
+        sessionStorage.setItem('chatname', respObj.name);
+    }
+
+    function checkExistChatSuccess() {
+        loginChatForm.roomName.disabled = true;
+        loginChatForm.userName.disabled = false;
+        
+        loginChatForm.nextButton.disabled = true;
+        
+        sessionStorage.setItem('key', respObj.key);
+        sessionStorage.setItem('chatname', respObj.name);
+    }
+
+    function loginChatSuccess(xhr) {
+        let respObj = JSON.parse(xhr.response);
+        console.dir(respObj);
+
+        loginChatForm.roomName.disabled = true;
+        loginChatForm.userName.disabled = true;
+        loginChatForm.roomLink.disabled = false;
+
+        sessionStorage.setItem('username', respObj.username);
+        sessionStorage.setItem('token', respObj.userid);
+
+
+        //elem.submit();
+    }
+
+    // ВАЛИДАЦИЯ
+    loginChatForm.roomName.oninput = function () {
+        if ( loginChatForm.roomName.value.length < 3 || loginChatForm.roomName.value.length > 50 )  {
+            loginChatForm.nextButton.disabled = true;
+        } else {
+            loginChatForm.nextButton.disabled = false;
+        }
+    }
+    loginChatForm.userName.oninput = function () {
+        if (loginChatForm.userName.value.length < 3 || loginChatForm.userName.value.length > 50) {
+            loginChatForm.nextButton.disabled = true;
+        } else {
+            loginChatForm.nextButton.disabled = false;
+        }
+    }
+
+    var first = true;
+
+    loginChatForm.onsubmit = function () {
+
+        if(first) {
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", SettingController.getUrl()+"api/chat/create/"+loginChatForm.roomName.value, true)
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        createChatSusccess(xhr);
+                        first = false;
+                    }
+                }
             }
-    );
+            xhr.send();
+        } else {
+            let from = new FormData();
+            from.append("name", loginChatForm.userName.value);
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", SettingController.getUrl()+"api/user/login/"+sessionStorage.getItem('key'), true)
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        loginChatSuccess(xhr);
+                    }
+                }
+            }
+            xhr.send(from);
+        }
 
 
+
+        return false;
+    }
 }
--- js/settingController.js
+++ js/settingController.js
@@ -1,8 +1,9 @@
 (function () {
     window.SettingController = class SettingController {
         static getUrl() {
-            const serverAdress = "http://158.46.83.151/easychatServer/";
+            // const serverAdress = "http://158.46.83.151/easychatServer/";
             // const serverAdress = "http://localhost:52834/";
+            const serverAdress = "http://127.0.0.1:8000/"
             return serverAdress;
         }
         static getNumberLimitMessage() {
